f31edba591838432b9ffa2ee3f831c46a679139a
RainsongTaurandira <50034435+RainsongTaurandira@users.noreply.github.com>
2022-05-31 15:18:19 -0300

    initial files
    HTML, CSS, and vanilla JavaScript.

---
+++ app.js
@@ -0,0 +1,23 @@
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+const subtotal = document.querySelector("input");
+
+
+button.addEventListener("click", function(){
+    let subtotal2 = subtotal.value;
+    let tax = (subtotal2*0.15).toFixed(2);
+    let total = (parseInt(tax) + parseInt(subtotal2)).toFixed(2);
+    //need to force it out of concatination using parseInt or parseFloat
+    
+    let taxBit = (tax*0.67).toFixed(2);
+    let final = total - taxBit;
+
+    let temp = `<p>On an invoice of <b>$${subtotal2}</b> <br> 
+    The HST is <b>$${tax}</b> <br> 
+    for a total of <b>$${total}</b> <br>  
+    Put this under the total (ie, subtracting 2/3 of the tax): <b> -$${taxBit} </b><br>
+    Draw two lines under the that, and<br>
+    write this under the two lines: <b>$${final}</b></p>`;
+    
+    output.innerHTML = temp;
+})
